43617d870f425c6f6f9279e3d02266ab43cedc9a
Andrew <haddada@uwplatt.edu>
2022-02-23 18:23:33 -0600

    Committing files for Gitlab issues #2 #3 #16 and #17

---
+++ src/InputGetter.java
@@ -0,0 +1,6 @@
+/**
+ * Input Getter implements the inputtable interface to output all the results and such of the battle pets game
+ * */
+public class InputGetter implements Inputtable {
+    Scanner read = new Scanner(System.in);
+}
---
+++ src/OutputGetter.java
@@ -0,0 +1,9 @@
+
+import java.util.List;
+
+/**
+ * Output Getter implements the outputtable interface to output all the results and such of the battle pets game
+ * */
+public class OutputGetter implements Outputtable{
+
+}
