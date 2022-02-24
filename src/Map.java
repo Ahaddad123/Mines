f0f01bc3429c70d4cca97de70fa4474aa21a2ad6
Jasmine Krahn <krahnja@uwplatt.edu>
2022-02-23 23:57:14 -0600

    issues #11 and #12

--- src/Map.java
+++ src/Map.java
@@ -6,7 +6,7 @@ public class Map {
 
    private int MAX_WIDTH = 5;
    private int MAX_HEIGHT = 3;
-   private Room[][] map;
+   private Room[][][] map;
    private List<Item> weapons;
    private List<Item> treasures;
 
@@ -14,29 +14,185 @@ public class Map {
    {
       this.weapons = weapons;
       this.treasures = treasures;
-      map = new Room[MAX_WIDTH][MAX_WIDTH];
-      firstFloorFirstRow();
+      // row, column, floor
+      map = new Room[MAX_WIDTH][MAX_WIDTH][MAX_HEIGHT];
+      firstFloor();
    }
 
-   public void printMap()
+   private void firstFloor()
    {
-      for(int i = 0; i < MAX_WIDTH; i++)
-      {
-         map[0][i].printDirections();
-      }
+      firstFloorFirstRow();
+      firstFloorSecondRow();
+      firstFloorThirdRow();
+      firstFloorForthRow();
+      firstFloorFifthRow();
    }
 
    private void firstFloorFirstRow()
    {
-      // row, column, floor
-      HashMap<Direction, Boolean> m = new HashMap<>();
-      m.put(Direction.NORTH, false);
-      HashMap<Monster, Direction> map1 = new HashMap<>();
-      map1.put(new Monster("name"), Direction.WEST);
-      List<Item> items = new ArrayList<>();
-      items.add(treasures.get(0));
-      map[0][0] = new Room(m, items, map1, "");
+      createRoom(false, true, false, false, false, false,
+              new Monster("Code in Notepad"), Direction.EAST,
+              null, null, null, null, null,
+              0, 0, "Video Card");
 
+      createRoom(false, false, false, false, false, false,
+              new Monster("Code in Notepad"), Direction.WEST,
+              null, null, treasures.get(0), treasures.get(1),
+              treasures.get(2), 1, 0, "Power Supply");
+
+      createRoom(false, false, true, false, false, false,
+              new Monster("Missing Semicolon"), Direction.SOUTH,
+              null, null, null, null,
+              null, 2, 0, "Motherboard");
+
+      createRoom(false, true, true, true, false, false,
+              null, null, null, null, weapons.get(2),
+              null, null, 3, 1, "RAM");
+
+      createRoom(false, false, false, true, false, false,
+              new Monster("Spelling Mistake"), Direction.SOUTH,
+              null, null, null, null,
+              null, 4, 0, "CPU");
+   }
+
+   private void firstFloorSecondRow()
+   {
+      createRoom(true, false, true, false, false, false,
+              new Monster("Failed Build"), Direction.SOUTH,
+              null, null, weapons.get(17), null, null,
+              0, 1, "Hard Drive");
+
+      createRoom(false, false, true, true, false, false,
+              null, null,
+              null, null, null, null,
+              null, 1, 1, "Solid-State Drive");
+
+      createRoom(false, true, false, true, false, false,
+              new Monster("Missing Semicolon"), Direction.NORTH,
+              null, null, null, null,
+              null, 2, 1, "Optical Disk Drive");
+
+      createRoom(true, true, false, false, false, false,
+              null, null, null, null, null,
+              null, null, 3, 1, "Monitor");
+
+      createRoom(false, false, false, false, false, false,
+              new Monster("Spelling Mistake"), Direction.NORTH,
+              new Monster("Wrong Operator"), Direction.SOUTH,
+              weapons.get(1), treasures.get(3),
+              treasures.get(4), 4, 1, "Keyboard");
+   }
+
+   private void firstFloorThirdRow()
+   {
+      createRoom(false, true, false, false, false, false,
+              new Monster("Failed Build"), Direction.NORTH,
+              null, null, null, null, null,
+              0, 2, "Mouse");
+
+      createRoom(false, false, false, false, false, false,
+              new Monster("Missing Method"), Direction.SOUTH,
+              null, null, treasures.get(5), treasures.get(6),
+              weapons.get(3), 1, 2, "Flash Drive");
+
+      createRoom(true, true, true, false, false, false,
+              null, null,
+              null, null, weapons.get(12), null,
+              null, 2, 2, "Printer");
+
+      createRoom(true, false, true, true, false, false,
+              null, null, null, null, null,
+              null, null, 3, 2, "Speakers");
+
+      createRoom(false, true, false, true, false, false,
+              new Monster("Wrong Operator"), Direction.NORTH,
+              null, null, null, null,
+              null, 4, 2, "Floppy Disk");
+   }
+
+   private void firstFloorForthRow()
+   {
+      createRoom(true, true, true, false, false, false,
+              null, null,
+              null, null, weapons.get(5), null, null,
+              0, 3, "Webcam");
+
+      createRoom(false, false, true, true, false, false,
+              new Monster("Missing Method"), Direction.NORTH,
+              null, null, null, null,
+              null, 1, 3, "Microphone");
+
+      createRoom(true, false, true, true, false, false,
+              null, null,
+              null, null, null, null,
+              null, 2, 3, "Modem");
+
+      createRoom(false, false, false, true, false, false,
+              new Monster("Magic Number"), Direction.SOUTH, null, null, treasures.get(7),
+              null, null, 3, 3, "Router");
+
+      createRoom(true, true, false, false, false, false,
+              null, null,
+              null, null, weapons.get(4), null,
+              null, 4, 3, "Network Switch");
+   }
+
+   private void firstFloorFifthRow()
+   {
+      createRoom(true, false, false, false, false, false,
+              new Monster("Wrong Variable Type"), Direction.EAST,
+              null, null, treasures.get(8), null, null,
+              0, 4, "Firewall");
+
+      createRoom(false, false, true, false, false, false,
+              new Monster("Wrong Variable Type"), Direction.WEST,
+              null, null, null, null,
+              null, 1, 4, "Fan");
+
+      createRoom(false, false, true, true, false, false,
+              null, null,
+              null, null, weapons.get(13), null,
+              null, 2, 4, "Battery");
+
+      createRoom(false, false, false, true, false, false,
+              new Monster("Magic Number"), Direction.NORTH, null, null,
+              treasures.get(9),
+              null, null, 3, 4, "Graphics Card");
+
+      createRoom(true, false, false, false, false, false,
+              null, null,
+              null, null, null, null,
+              null, 4, 4, "Operating System");
+   }
+
+   private void createRoom(boolean n, boolean s, boolean e, boolean w,
+                           boolean u, boolean d, Monster monster1,
+                      Direction direction1, Monster monster2,
+                      Direction direction2,
+                      Item item1, Item item2, Item item3, int i, int j,
+                      String room)
+   {
+      HashMap<Direction, Boolean> db= new HashMap<>();
+      db.put(Direction.NORTH, n);
+      db.put(Direction.SOUTH, s);
+      db.put(Direction.EAST, e);
+      db.put(Direction.WEST, w);
+      db.put(Direction.UP, u);
+      db.put(Direction.DOWN, d);
+      HashMap<Monster, Direction> md = new HashMap<>();
+      if(monster1 != null)
+         md.put(monster1, direction1);
+      if(monster2 != null)
+         md.put(monster2, direction2);
+      List<Item> items = new ArrayList<>();
+      if(item1 != null)
+         items.add(item1);
+      if(item2 != null)
+         items.add(item2);
+      if(item3 != null)
+         items.add(item3);
+      map[i][j][0] = new Room(db, items, md, "You have entered the " +
+              room + ".");
    }
 
 }
--- src/Room.java
+++ src/Room.java
@@ -5,18 +5,30 @@ public class Room {
    private HashMap<Direction, Boolean> directions;
    private List<Item> items;
    private HashMap<Monster, Direction> monsters;
+   private String description;
 
    Room(HashMap<Direction, Boolean> directions, List<Item> items,
         HashMap<Monster, Direction> monsters, String description)
    {
       this.items = items;
+      this.monsters = monsters;
+      this.description = description;
+      this.directions = directions;
    }
 
-   public void setOpenDirections() {
-      for(Monster monster : monsters) {
-         String passage = (String) monster.getBlocking().get(new Room());
-      }
+   public HashMap<Direction, Boolean> getDirections() {
+      return directions;
    }
 
+   public List<Item> getItems() {
+      return items;
+   }
 
+   public HashMap<Monster, Direction> getMonsters() {
+      return monsters;
+   }
+
+   public String getDescription() {
+      return description;
+   }
 }
