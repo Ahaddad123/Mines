f28617bfc065c8f5e4b6471d057f71f1fb7ce1ce
Makenna Halvensleben <halvenslebem@uwplatt.com>
2022-02-24 20:07:29 -0600

    Issue #7 : Created Player class with location and inventory attributes.

--- src/main.java
+++ src/main.java
@@ -1,2 +1,10 @@
+/**
+ * @author Makenna Halvensleben
+ * Main class that runs the main method.
+ */
 public class main {
+    public static void main(String[] args) {
+        GameController gameController = new GameController();
+        gameController.run();
+    }
 }
