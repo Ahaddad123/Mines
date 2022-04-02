8a321eaadbaa464ab1ab82feece0726d4b3d0c0f
Jasmine Krahn <krahnja@uwplatt.edu>
2022-04-02 13:16:21 -0500

    issues #11 and #12

--- src/Map.java
+++ src/Map.java
@@ -13,29 +13,32 @@ public class Map {
 
    private int MAX_WIDTH = 5;
    private int MAX_HEIGHT = 3;
-   private Room[][] map;
+   private Room[][][] map;
    private List<Item> weapons;
    private List<Item> treasures;
+   private List<Item> monsters;
 
    /**
     * Constructor for map
     * @param weapons list of all weapons
     * @param treasures list of all treasures
     */
-   Map(List<Item> weapons, List<Item> treasures)
+   Map(List<Item> weapons, List<Item> treasures, List<Item> monsters)
    {
       this.weapons = weapons;
       this.treasures = treasures;
+      this.monsters = monsters;
       // row, column, floor
-      map = new Room[MAX_WIDTH][MAX_WIDTH];
+      map = new Room[MAX_WIDTH][MAX_WIDTH][MAX_HEIGHT];
       firstFloor();
+      secondFloor();
    }
 
    /**
     * gets game map
     * @return game map
     */
-   public Room[][] getMap() {
+   public Room[][][] getMap() {
       return map;
    }
 
@@ -59,10 +62,11 @@ public class Map {
     * @param room room description
     */
    private void createRoom(boolean n, boolean s, boolean e, boolean w,
-                           boolean u, boolean d, Monster monster1,
-                           Commands direction1, Monster monster2,
+                           boolean u, boolean d, Item monster1,
+                           Commands direction1, Item monster2,
                            Commands direction2,
-                           Item item1, Item item2, Item item3, int i, int j,
+                           Item item1, Item item2, Item item3, int i, int j
+           , int k,
                            String room)
    {
       HashMap<Commands, Boolean> db= new HashMap<>();
@@ -84,7 +88,7 @@ public class Map {
          items.add(item2);
       if(item3 != null)
          items.add(item3);
-      map[i][j] = new Room(db, items, md, i, j, "You have entered the" +
+      map[i][j][k] = new Room(db, items, md, i, j, k, "You have entered the" +
               " " +
               room + ".");
    }
@@ -102,33 +106,45 @@ public class Map {
    }
 
    /**
+    * builds first floor of the game map
+    */
+   private void secondFloor()
+   {
+      secondFloorFirstRow();
+      secondFloorSecondRow();
+      secondFloorThirdRow();
+      secondFloorForthRow();
+      secondFloorFifthRow();
+   }
+
+   /**
     * builds first row of first floor
     */
    private void firstFloorFirstRow()
    {
-      createRoom(false, true, false, false, false, false,
-              new Monster("Code in Notepad"), Commands.EAST,
+      createRoom(false, true, false, false, true, false,
+              monsters.get(0), Commands.EAST,
               null, null, null, null, null,
-              0, 0, "Video Card");
+              0, 0, 0,"Video Card");
 
       createRoom(false, false, false, false, false, false,
-              new Monster("Code in Notepad"), Commands.WEST,
+              monsters.get(0), Commands.WEST,
               null, null, treasures.get(0), treasures.get(1),
-              treasures.get(2), 1, 0, "Power Supply");
+              treasures.get(2), 1, 0, 0,"Power Supply");
 
       createRoom(false, false, true, false, false, false,
-              new Monster("Missing Semicolon"), Commands.SOUTH,
+              monsters.get(1), Commands.SOUTH,
               null, null, null, null,
-              null, 2, 0, "Motherboard");
+              null, 2, 0, 0,"Motherboard");
 
       createRoom(false, true, true, true, false, false,
               null, null, null, null, weapons.get(2),
-              null, null, 3, 0, "RAM");
+              null, null, 3, 0, 0, "RAM");
 
       createRoom(false, false, false, true, false, false,
-              new Monster("Spelling Mistake"), Commands.SOUTH,
+              monsters.get(2), Commands.SOUTH,
               null, null, null, null,
-              null, 4, 0, "CPU");
+              null, 4, 0, 0,"CPU");
    }
 
    /**
@@ -137,29 +153,29 @@ public class Map {
    private void firstFloorSecondRow()
    {
       createRoom(true, false, true, false, false, false,
-              new Monster("Failed Build"), Commands.SOUTH,
+              monsters.get(7), Commands.SOUTH,
               null, null, weapons.get(17), null, null,
-              0, 1, "Hard Drive");
+              0, 1, 0,"Hard Drive");
 
       createRoom(false, false, true, true, false, false,
               null, null,
               null, null, null, null,
-              null, 1, 1, "Solid-State Drive");
+              null, 1, 1, 0,"Solid-State Drive");
 
       createRoom(false, true, false, true, false, false,
-              new Monster("Missing Semicolon"), Commands.NORTH,
+              monsters.get(1), Commands.NORTH,
               null, null, null, null,
-              null, 2, 1, "Optical Disk Drive");
+              null, 2, 1, 0,"Optical Disk Drive");
 
       createRoom(true, true, false, false, false, false,
               null, null, null, null, null,
-              null, null, 3, 1, "Monitor");
+              null, null, 3, 1, 0,"Monitor");
 
       createRoom(false, false, false, false, false, false,
-              new Monster("Spelling Mistake"), Commands.NORTH,
-              new Monster("Wrong Operator"), Commands.SOUTH,
+              monsters.get(2), Commands.NORTH,
+              monsters.get(3), Commands.SOUTH,
               weapons.get(1), treasures.get(3),
-              treasures.get(4), 4, 1, "Keyboard");
+              treasures.get(4), 4, 1, 0,"Keyboard");
    }
 
    /**
@@ -168,28 +184,28 @@ public class Map {
    private void firstFloorThirdRow()
    {
       createRoom(false, true, false, false, false, false,
-              new Monster("Failed Build"), Commands.NORTH,
+              monsters.get(7), Commands.NORTH,
               null, null, null, null, null,
-              0, 2, "Mouse");
+              0, 2, 0,"Mouse");
 
       createRoom(false, false, false, false, false, false,
-              new Monster("Missing Method"), Commands.SOUTH,
+              monsters.get(6), Commands.SOUTH,
               null, null, treasures.get(5), treasures.get(6),
-              weapons.get(3), 1, 2, "Flash Drive");
+              weapons.get(3), 1, 2, 0,"Flash Drive");
 
       createRoom(true, true, true, false, false, false,
               null, null,
               null, null, weapons.get(12), null,
-              null, 2, 2, "Printer");
+              null, 2, 2, 0,"Printer");
 
       createRoom(true, false, true, true, false, false,
               null, null, null, null, null,
-              null, null, 3, 2, "Speakers");
+              null, null, 3, 2, 0,"Speakers");
 
       createRoom(false, true, false, true, false, false,
-              new Monster("Wrong Operator"), Commands.NORTH,
+              monsters.get(3), Commands.NORTH,
               null, null, null, null,
-              null, 4, 2, "Floppy Disk");
+              null, 4, 2, 0,"Floppy Disk");
    }
 
    /**
@@ -200,26 +216,26 @@ public class Map {
       createRoom(true, true, true, false, false, false,
               null, null,
               null, null, weapons.get(5), null, null,
-              0, 3, "Webcam");
+              0, 3, 0,"Webcam");
 
       createRoom(false, false, true, true, false, false,
-              new Monster("Missing Method"), Commands.NORTH,
+              monsters.get(6), Commands.NORTH,
               null, null, null, null,
-              null, 1, 3, "Microphone");
+              null, 1, 3, 0,"Microphone");
 
       createRoom(true, false, true, true, false, false,
               null, null,
               null, null, null, null,
-              null, 2, 3, "Modem");
+              null, 2, 3, 0,"Modem");
 
       createRoom(false, false, false, true, false, false,
-              new Monster("Magic Number"), Commands.SOUTH, null, null, treasures.get(7),
-              null, null, 3, 3, "Router");
+              monsters.get(4), Commands.SOUTH, null, null, treasures.get(7),
+              null, null, 3, 3, 0,"Router");
 
       createRoom(true, true, false, false, false, false,
               null, null,
               null, null, weapons.get(4), null,
-              null, 4, 3, "Network Switch");
+              null, 4, 3, 0,"Network Switch");
    }
 
    /**
@@ -228,28 +244,343 @@ public class Map {
    private void firstFloorFifthRow()
    {
       createRoom(true, false, false, false, false, false,
-              new Monster("Wrong Variable Type"), Commands.EAST,
+              monsters.get(5), Commands.EAST,
               null, null, treasures.get(8), null, null,
-              0, 4, "Firewall");
+              0, 4, 0,"Firewall");
 
       createRoom(false, false, true, false, false, false,
-              new Monster("Wrong Variable Type"), Commands.WEST,
+              monsters.get(5), Commands.WEST,
               null, null, null, null,
-              null, 1, 4, "Fan");
+              null, 1, 4, 0,"Fan");
 
       createRoom(false, false, true, true, false, false,
               null, null,
               null, null, weapons.get(13), null,
-              null, 2, 4, "Battery");
+              null, 2, 4, 0,"Battery");
 
       createRoom(false, false, false, true, false, false,
-              new Monster("Magic Number"), Commands.NORTH, null, null,
+              monsters.get(4), Commands.NORTH, null, null,
               treasures.get(9),
-              null, null, 3, 4, "Graphics Card");
+              null, null, 3, 4, 0,"Graphics Card");
 
-      createRoom(true, false, false, false, false, false,
+      createRoom(true, false, false, false, true, false,
               null, null,
               null, null, null, null,
-              null, 4, 4, "Operating System");
+              null, 4, 4, 0,"Operating System");
+   }
+
+   /**
+    * builds first row of second floor
+    */
+   private void secondFloorFirstRow()
+   {
+      createRoom(false, false, true, false, false, true,
+              monsters.get(14), Commands.SOUTH,
+              null, null, weapons.get(7), null, null,
+              0, 0, 1,"");
+
+      createRoom(false, false, true, false,false, false,
+              monsters.get(13), Commands.EAST,
+              null, null, null, null,
+              null, 1, 0, 1,"");
+
+      createRoom(false, false, false, true, false, false,
+              monsters.get(13), Commands.WEST,
+              null, null, treasures.get(10), null,
+              null, 2, 0, 1,"");
+
+      createRoom(false, false, true, true, false, false,
+              null, null, null, null,
+              null,
+              null, null, 3, 0, 1,"");
+
+      createRoom(false, true, false, true, false, false,
+              null, null,
+              null, null, weapons.get(8), null,
+              null, 4, 0, 1,"");
+   }
+
+   /**
+    * builds second row of second floor
+    */
+   private void secondFloorSecondRow()
+   {
+      createRoom(false, true, false, false, false, false,
+              monsters.get(14), Commands.NORTH,
+              null, null, null, null, null,
+              0, 1, 1,"");
+
+      createRoom(false, true, true, false,false, false,
+              null, null,
+              null, null, weapons.get(23), null,
+              null, 1, 1, 1,"");
+
+      createRoom(false, true, false, true, false, false,
+              monsters.get(10), Commands.EAST,
+              null, null, null, null,
+              null, 2, 1, 1,"");
+
+      createRoom(false, true, true, false, false, false,
+              monsters.get(10), Commands.WEST, null, null,
+              null,
+              null, null, 3, 1, 1,"");
+
+      createRoom(true, false, false, true, false, false,
+              monsters.get(9), Commands.SOUTH,
+              null, null, null, null,
+              null, 4, 1, 1,"");
+   }
+
+   /**
+    * builds third row of second floor
+    */
+   private void secondFloorThirdRow()
+   {
+      createRoom(true, true, false, false, false, false,
+              null, null,
+              null, null, weapons.get(9), null, null,
+              0, 2, 1,"");
+
+      createRoom(true, false, false, false,false, false,
+              monsters.get(11), Commands.SOUTH,
+              null, null, null, null,
+              null, 1, 2, 1,"");
+
+      createRoom(true, false, false, false, true, false,
+              null, null,
+              null, null, treasures.get(11), null,
+              null, 2, 2, 1,"");
+
+      createRoom(true, true, false, false, false, false,
+              null, null, null, null,
+              weapons.get(15),
+              null, null, 3, 2, 1,"");
+
+      createRoom(false, true, false, false, false, false,
+              monsters.get(9), Commands.NORTH,
+              null, null, weapons.get(16), treasures.get(12),
+              null, 4, 2, 1,"");
+   }
+
+   /**
+    * builds forth row of second floor
+    */
+   private void secondFloorForthRow()
+   {
+      createRoom(true, false, false, false, false, false,
+              monsters.get(15), Commands.SOUTH,
+              null, null, treasures.get(13), null, null,
+              0, 3, 1,"");
+
+      createRoom(false, true, true, false,false, false,
+              monsters.get(11), Commands.NORTH,
+              null, null, null, null,
+              null, 1, 3, 1,"");
+
+      createRoom(false, false, true, true, false, false,
+              null, null,
+              null, null, null, null,
+              null, 2, 3, 1,"");
+
+      createRoom(true, false, false, true, false, false,
+              null, null, null, null,
+              treasures.get(14),
+              null, null, 3, 3, 1,"");
+
+      createRoom(true, false, false, false, false, false,
+              monsters.get(7), Commands.SOUTH,
+              null, null, weapons.get(21), null,
+              null, 4, 3, 1,"");
+   }
+
+
+   /**
+    * builds fifth row of second floor
+    */
+   private void secondFloorFifthRow()
+   {
+      createRoom(false, false, true, false, false, false,
+              monsters.get(15), Commands.NORTH,
+              null, null, weapons.get(6), null, null,
+              0, 4, 1,"");
+
+      createRoom(true, false, false, true,false, false,
+              monsters.get(12), Commands.EAST,
+              null, null, null, null,
+              null, 1, 4, 1,"");
+
+      createRoom(false, false, true, true, false, false,
+              monsters.get(12), Commands.WEST,
+              null, null, null, null,
+              null, 2, 4, 1,"");
+
+      createRoom(false, false, true, true, false, false,
+              null, null, null, null,
+              weapons.get(10),
+              null, null, 3, 4, 1,"");
+
+      createRoom(false, false, false, true, false, true,
+              monsters.get(7), Commands.NORTH,
+              null, null, null, null,
+              null, 4, 4, 1,"");
+   }
+
+   /**
+    * builds first row of third floor
+    */
+   private void thirdFloorFirstRow()
+   {
+      createRoom(false, true, false, false, false, false,
+              null, null,
+              null, null, weapons.get(18), null, null,
+              0, 1, 2,"");
+
+      createRoom(false, false, true, false,false, false,
+              monsters.get(20), Commands.SOUTH,
+              null, null, null, null,
+              null, 1, 1, 2,"");
+
+      createRoom(false, true, true, true, false, false,
+              null, null,
+              null, null, null, null,
+              null, 2, 1, 2,"");
+
+      createRoom(false, false, true, true, false, false,
+              monsters.get(17), Commands.SOUTH, null, null,
+              null,
+              null, null, 3, 1, 2,"");
+
+      createRoom(false, true, false, true, false, false,
+              null, null,
+              null, null, treasures.get(15), null,
+              null, 4, 1, 2,"");
+   }
+
+   /**
+    * builds second row of third floor
+    */
+   private void thirdFloorSecondRow()
+   {
+      createRoom(true, true, false, false, false, false,
+              monsters.get(21), Commands.EAST,
+              null, null, null, null, null,
+              0, 2, 2,"");
+
+      createRoom(false, false, false, false,false, false,
+              monsters.get(20), Commands.NORTH,
+              monsters.get(21), Commands.WEST, treasures.get(16),
+              treasures.get(17),
+              null, 1, 2, 2,"");
+
+      createRoom(true, true, false, false, false, false,
+              null, null,
+              null, null, weapons.get(22), null,
+              null, 2, 2, 2,"");
+
+      createRoom(false, false, false, false, false, false,
+              monsters.get(17), Commands.NORTH, null, null,
+              treasures.get(18),
+              treasures.get(19), null, 3, 2, 2,"");
+
+      createRoom(true, false, false, false, false, false,
+              monsters.get(23), Commands.SOUTH,
+              null, null, null, null,
+              null, 4, 2, 2,"");
+   }
+
+   /**
+    * builds third row of third floor
+    */
+   private void thirdFloorThirdRow()
+   {
+      createRoom(true, true, false, false, false, false,
+              null, null,
+              null, null, treasures.get(20), null, null,
+              0, 3, 2,"");
+
+      createRoom(false, false, false, true,false, false,
+              null, null,
+              null, null, weapons.get(11),
+              null,
+              null, 1, 3, 2,"");
+
+      createRoom(true, true, true, true, false, true,
+              null, null,
+              null, null, null, null,
+              null, 2, 3, 2,"");
+
+      createRoom(false, false, true, true, false, false,
+              null, null, null, null,
+              null,
+              null, null, 3, 3, 2,"");
+
+      createRoom(false, false, false, true, false, false,
+              monsters.get(23), Commands.NORTH,
+              monsters.get(22), Commands.SOUTH, weapons.get(19), null,
+              null, 4, 3, 2,"");
+   }
+
+   /**
+    * builds forth row of third floor
+    */
+   private void thirdFloorForthRow()
+   {
+      createRoom(true, true, false, false, false, false,
+              monsters.get(18), Commands.EAST,
+              null, null, null, null, null,
+              0, 3, 2,"");
+
+      createRoom(false, true, false, false,false, false,
+              monsters.get(18), Commands.WEST,
+              null, null, treasures.get(21),
+              null,
+              null, 1, 3, 2,"");
+
+      createRoom(true, false, false, false, false, false,
+              monsters.get(19), Commands.SOUTH,
+              null, null, null, null,
+              null, 2, 3, 2,"");
+
+      createRoom(false, false, false, false, false, false,
+              monsters.get(16), Commands.EAST, null, null,
+              weapons.get(0),
+              treasures.get(22), null, 3, 3, 2,"");
+
+      createRoom(false, true, false, false, false, false,
+              monsters.get(22), Commands.NORTH,
+              null, null, null, null,
+              null, 4, 3, 2,"");
+   }
+
+   /**
+    * builds fifth row of third floor
+    */
+   private void thirdFloorFifthRow()
+   {
+      createRoom(true, false, true, false, false, false,
+              null, null,
+              null, null, weapons.get(14), null, null,
+              0, 4, 2,"");
+
+      createRoom(true, false, false, true,false, false,
+              null, null,
+              null, null, null,
+              null,
+              null, 1, 4, 2,"");
+
+      createRoom(false, false, true, false, false, false,
+              monsters.get(19), Commands.NORTH,
+              null, null, null, null,
+              null, 2, 4, 2,"");
+
+      createRoom(false, false, true, true, false, false,
+              null, null, null, null,
+              null,
+              null, null, 3, 4, 2,"");
+
+      createRoom(true, false, false, true, false, false,
+              null, null,
+              null, null, weapons.get(20), null,
+              null, 4, 4, 2,"");
    }
 }
