0f43dbf71315ae3c96aa5a85ffea7cb362019418
Makenna Halvensleben <halvenslebem@uwplatt.edu>
2022-04-19 20:58:17 -0500

    Issue #27

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -5,7 +5,7 @@ public class PlayerTest {
     private final Player player = new Player(new Room(1, 1, 1), 1, 1, 1);
 
     @Test
-    public void move_NORTH() {
+    public void testMove_NORTH() {
         player.move(Commands.NORTH, new Map());
         Assertions.assertEquals(1, player.getXIndex());
         Assertions.assertEquals(0, player.getYIndex());
@@ -13,7 +13,7 @@ public class PlayerTest {
     }
 
     @Test
-    public void move_SOUTH() {
+    public void testMove_SOUTH() {
         player.move(Commands.SOUTH, new Map());
         Assertions.assertEquals(1, player.getXIndex());
         Assertions.assertEquals(2, player.getYIndex());
@@ -21,7 +21,7 @@ public class PlayerTest {
     }
 
     @Test
-    public void move_EAST() {
+    public void testMove_EAST() {
         player.move(Commands.EAST, new Map());
         Assertions.assertEquals(2, player.getXIndex());
         Assertions.assertEquals(1, player.getYIndex());
@@ -29,7 +29,7 @@ public class PlayerTest {
     }
 
     @Test
-    public void move_WEST() {
+    public void testMove_WEST() {
         player.move(Commands.WEST, new Map());
         Assertions.assertEquals(0, player.getXIndex());
         Assertions.assertEquals(1, player.getYIndex());
@@ -37,7 +37,7 @@ public class PlayerTest {
     }
 
     @Test
-    public void move_UP() {
+    public void testMove_UP() {
         player.move(Commands.UP, new Map());
         Assertions.assertEquals(1, player.getXIndex());
         Assertions.assertEquals(1, player.getYIndex());
@@ -45,7 +45,7 @@ public class PlayerTest {
     }
 
     @Test
-    public void move_DOWN() {
+    public void testMove_DOWN() {
         player.move(Commands.DOWN, new Map());
         Assertions.assertEquals(1, player.getXIndex());
         Assertions.assertEquals(1, player.getYIndex());
